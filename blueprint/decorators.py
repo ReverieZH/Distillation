c66ff8e4bc3190cbf7f802d00f8791d65c9886e7
reveriezh <1344980837@qq.com>
2022-06-14 10:22:45 +0800

    Perfect registration login and return value

--- blueprint/decorators.py
+++ blueprint/decorators.py
@@ -5,6 +5,7 @@ from functools import wraps
 from werkzeug.local import LocalProxy
 from flask import request, jsonify, _request_ctx_stack, current_app
 from blueprint import RETCODE
+from .utils import gen_response_data
 import re
 
 current_identity = LocalProxy(lambda: getattr(_request_ctx_stack.top, 'current_identity', None))
@@ -51,22 +52,26 @@ def jwt_required(fn):
     def wapper(*args, **kwargs):
         auth_header_value = request.headers.get('Authorization', None)
         if not auth_header_value:
-            return jsonify(code=RETCODE.NOAUTH, msg='Authorization缺失')
+            response_data = gen_response_data(RETCODE.NOAUTH, 'Authorization缺失')
+            return jsonify(response_data)
         parts = auth_header_value.split('.')
         if len(parts) == 1:
-            return jsonify(code=RETCODE.NOTOKEN, msg='Token缺失')
+            response_data = gen_response_data(RETCODE.NOTOKEN, 'Token缺失')
+            return jsonify(response_data)
         elif len(parts) > 3:
-            return jsonify(code=RETCODE.TOKENERR, msg='Token无效')
+            response_data = gen_response_data(RETCODE.TOKENERR, 'Token无效')
+            return jsonify(response_data)
         token = auth_header_value
         try:
             payload = jwt_decode(token)
         except jwt.InvalidTokenError as e:
-            return jsonify(code=RETCODE.INVALID_TOKEN, msg=str(e))
+            response_data = gen_response_data(RETCODE.INVALID_TOKEN, str(e))
+            return jsonify(response_data)
         _request_ctx_stack.top.current_identity = payload.get('identity')
         if payload.get('identity') is None:
-            return jsonify(code=RETCODE.NOLOGIN, msg='用户不存在')
+            response_data = gen_response_data(RETCODE.NOLOGIN, '用户不存在')
+            return jsonify(response_data)
         return fn(*args, **kwargs)
-
     return wapper
 
 
@@ -79,13 +84,15 @@ def register_required(fn):
         phone = request.json.get('phone', None)
         r_code = request.json.get('code', None)
         if username is None or password is None or confirm_password is None or phone is None or r_code is None:
-            return jsonify(code=RETCODE.PARAMERR, msg='您有信息未填写')
+            response_data = gen_response_data(RETCODE.PARAMERR, '您有信息未填写')
+            return jsonify(response_data)
         phone_match = re.search(re.compile(r"1[356789]\d{9}"), phone)
         password_match = re.search(re.compile(r'[0-9a-zA-Z]{8,10}'), password)
         if phone_match is None:
-            return jsonify(code=RETCODE.PARAMERR, msg='手机号格式不正确')
+            response_data = gen_response_data(RETCODE.PARAMERR, '手机号格式不正确')
+            return jsonify(response_data)
         if password_match is None:
-            return jsonify(code=RETCODE.PARAMERR, msg='密码格式不正确,包含数字、大写或小写字母8到16位')
+            response_data = gen_response_data(RETCODE.PARAMERR, '密码格式不正确,包含数字、大写或小写字母8到16位')
+            return jsonify(response_data)
         return fn(*args, **kwargs)
-
     return wapper
