f610702592625eec67b1a3f64c0a17987f366cd3
Asteriska <1758995741@qq.com>
2022-07-02 00:04:22 +0800

    completed the config.

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from datetime import timedelta
 
 JSON_AS_ASCII = False
 # 数据库的配置信息
-HOSTNAME = '127.0.0.1'
-PORT = '3306'
+HOSTNAME = 'bj-cynosdbmysql-grp-ifso5nx8.sql.tencentcdb.com'
+PORT = '29050'
 DATABASE = 'distillation'
-USERNAME = 'root'
-PASSWORD = 'ipmi123456'
+USERNAME = 'devlop'
+PASSWORD = 'ipmi123456!'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
@@ -15,8 +15,9 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = "ipmidistillationkdyZRD7RdK5kXO0s"
 
 # redis
-REDIS_HOST = '127.0.0.1'
+REDIS_HOST = '124.223.222.243'
 REDIS_PORT = 6379
+REDIS_PWD = 'cnsoftbei'
 
 # JWT
 JWT_SECRET_KEY = ""
@@ -31,10 +32,14 @@ JWT_AUTH_HEADER_PREFIX = 'JWT'
 JWT_NOT_BEFORE_DELTA = timedelta(seconds=0)
 
 # 腾讯云存储
-secret_id = 'AKIDV2HEsEH1ScU3IGwJN8bpmQnrd6IDqBDx'
-secret_key = 'UgaELm27zc1A4i7GBqFPJxLSO4TbU6QU'
+secret_id = 'AKIDyoPmKekaBXdU6TaS5vWs0sUqvlLfisnT'
+secret_key = 'SehX9ZpshHckfLl5JWYK3hQ2q0xq5JVV'
 region = 'ap-chengdu'
 token = None
 scheme = 'https'
 
 bucket = 'distillation-1312273849'
+
+# 阿里云
+AccessKey_ID = 'LTAI5tQi9guVXokvKH2h2GZb'
+AccessKey_Secret = 'nThznxN2fE5aLiVZYavszeVT9pk7ZK'
