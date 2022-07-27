97c4f7af29a5637835ffddaa4132b1720e5e6460
reveriezh <1344980837@qq.com>
2022-07-27 17:29:26 +0800

    modify password bug

--- blueprint/decorators.py
+++ blueprint/decorators.py
@@ -108,7 +108,7 @@ def modify_password_required(fn):
         if new_password is None :
             response_data = gen_response_data(RETCODE.PARAMERR, '请输入修改的密码')
             return jsonify(response_data)
-        password_match = re.search(re.compile(r'[0-9a-zA-Z]{8,10}'), new_password)
+        password_match = re.search(re.compile(r'[0-9a-zA-Z]{8,16}'), new_password)
         if password_match is None:
             response_data = gen_response_data(RETCODE.PARAMERR, '密码格式不正确,包含数字、大写或小写字母8到16位')
             return jsonify(response_data)
